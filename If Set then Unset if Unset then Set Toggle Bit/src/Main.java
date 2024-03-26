fd4efb5f60791bab88d49fe82c01b7f90642064b
Ace <152076508+AkshayDilipYadav@users.noreply.github.com>
2024-03-27 00:24:34 +0530

    Add files via upload

---
+++ If Set then Unset if Unset then Set Toggle Bit/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Main {
+
+    /*
+    * Using XOR left Shift
+    * */
+
+    public static String getBits(int A){
+        char[] bits = new char[32];
+        for (int i = 31; i >= 0; i--){
+            int bit = (A>>i)&1;
+            bits[31-i] = (char)(bit + '0');
+        }
+        return new String(bits);
+    }
+    public static int orLeftShift(int A, int B){
+        int result  =A ^ (1 << B);
+        return result;
+    }
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+
+        int B = sc.nextInt();
+        int result = orLeftShift(A,B);
+
+        String bitsOf = getBits(A);
+        System.out.println ("If B bit is unset then set and if B Bit is set then unset "+result);
+        System.out.println(bitsOf);
+
+        String bitsOfResult = getBits(result);
+        System.out.println(bitsOfResult);
+
+    }
+}
